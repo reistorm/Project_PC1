bb4992a08e13fd517fceb7d9660be3e3be05d031
reistorm <rysaeva96@mail.ru>
2025-11-11 13:52:22 +0300

    task3_js

--- js/timer.js
+++ js/timer.js
@@ -7,7 +7,7 @@ const daysBlock = document.querySelector('.timer__days');
 let interval;
 
 
-const numWord = (value, words) => {
+const numWordSeconds = (value, words) => {
     value = Math.abs(value) % 100;
     const lastNum = value % 10;
 
@@ -18,10 +18,42 @@ const numWord = (value, words) => {
     return words[2];
 }
 
+const numWordMinutes = (value, words) => {
+    value = Math.abs(value) % 100;
+    const lastNum = value % 10;
+
+    if (value > 10 & value < 20) return words[2]
+    if (lastNum > 1 && lastNum < 5) return words[1]
+    if (lastNum === 1) return words[0]
+
+    return words[2];
+}
+
+const numWordHours = (value, words) => {
+    value = Math.abs(value) % 100;
+    const lastNum = value % 10;
+
+    if (value > 10 & value < 20) return words[2]
+    if (lastNum > 1 && lastNum < 5) return words[1]
+    if (lastNum === 1) return words[0]
+
+    return words[2];
+}
+
+const numWordDays = (value, words) => {
+    value = Math.abs(value) % 100;
+    const lastNum = value % 10;
+
+    if (value > 10 & value < 20) return words[2]
+    if (lastNum > 1 && lastNum < 5) return words[1]
+    if (lastNum === 1) return words[0]
+
+    return words[2];
+}
 
 const updateTimer = () => {
     const date = new Date();
-    const dateDeadline = new Date('14 november 2025').getTime();
+    const dateDeadline = new Date('11 november 2025').getTime();
     const timeRemaining = (dateDeadline - date) / 1000;
 
     const days = Math.floor((timeRemaining / 3600) / 24);
@@ -41,15 +73,27 @@ const updateTimer = () => {
     minutesBlock.textContent = fMinutes;
     secondsBlock.textContent = fSeconds;
 
-    secondsBlock.nextElementSibling.textContent = numWord(seconds, ['секунда', 'секунды', 'секунд']);
+    secondsBlock.nextElementSibling.textContent = numWordSeconds(seconds, ['секунда', 'секунды', 'секунд']);
+    minutesBlock.nextElementSibling.textContent = numWordMinutes(minutes, ['минута', 'минуты', 'минут']);
+    hoursBlock.nextElementSibling.textContent = numWordHours(hours, ['час', 'часа', 'часов']);
+    daysBlock.nextElementSibling.textContent = numWordDays(days, ['день', 'дня', 'дней'])
 
     if (timeRemaining <= 0) {
         clearInterval(interval);
         daysBlock.textContent = '00';
+        daysBlock.style.color = 'red';
+        daysBlock.nextElementSibling.style.color = 'red';
         hoursBlock.textContent = '00';
+        hoursBlock.style.color = 'red';
+        hoursBlock.nextElementSibling.style.color = 'red';
         minutesBlock.textContent = '00';
+        minutesBlock.style.color = 'red';
+        minutesBlock.nextElementSibling.style.color = 'red';
         secondsBlock.textContent = '00';
+        secondsBlock.style.color = 'red';
+        secondsBlock.nextElementSibling.style.color = 'red';
     }
+
 }
 
 updateTimer();
